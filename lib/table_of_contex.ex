77e920abc84a30f9637369b1960d7a6b0ff5cf26
Stephen Hara <stephen.hara@stephenhara.com>
2021-02-10 12:19:31 +0900

    initial commit, freshly generated

---
+++ lib/table_of_contex.ex
@@ -0,0 +1,18 @@
+defmodule TableOfContex do
+  @moduledoc """
+  Documentation for `TableOfContex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TableOfContex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/table_of_contex_test.exs
@@ -0,0 +1,8 @@
+defmodule TableOfContexTest do
+  use ExUnit.Case
+  doctest TableOfContex
+
+  test "greets the world" do
+    assert TableOfContex.hello() == :world
+  end
+end
